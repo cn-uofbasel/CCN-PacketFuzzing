57f7b354528e1702eb95c6d7e30bad450d31ccf2
phoeft670 <p.hoeft@stud.unibas.ch>
2018-05-03 09:45:04 +0200

    Adapted Interest to new Structure

--- Generation/Package.py
+++ Generation/Package.py
@@ -1,6 +1,15 @@
 class Package:
 
     def __init__(self, name, type, len, rlen, subpackages=None):
+        """Represents a NDN Package with all necesary data to randomize length and value
+            this class is for generation purpose and will be converted into a "real" NDN-Package
+
+            name -- Name of the Package, for info
+            type -- the number representing the package type in the tlv format
+            len -- the length which will be written into the the packet
+            rlen -- the real byte length of the package
+            subpackages -- list of recursive subpackages this package should contain
+            """
         if subpackages != None:
             self.rlen = 0
             for p in subpackages:
@@ -16,7 +25,7 @@ class Package:
         type = str(self.type)
         len = str(self.len)
         rlen = str(self.rlen)
-        string = self.name + "(" + type + ")" + "\n" + "written length: " + len + "\n" + "real length: " + rlen + "\n"
+        string = self.name + "(" + type + ")" + "| written length: " + len + "| real length: " + rlen + "\n"
         if self.subpackages != None:
             subpackages = str(self.subpackages)
             string += "with subpackages " + subpackages
