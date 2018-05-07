7a1cf9016b3ea844018c52142badca07d7a5bd5c
phoeft <p.hoeft@stud.unibas.ch>
2018-05-07 13:49:49 +0200

    changed programm invoke from os.system to subprocess

--- CCNStart/StartCCN.py
+++ CCNStart/StartCCN.py
@@ -1,10 +1,14 @@
-import os
-
+import subprocess
+from subprocess import PIPE,CalledProcessError,Popen
 class StartCCN:
-    def __init__(self):
-        os.system("start cmd")
-        os.chdir("C:\\Users\\timko\\Documents\\Github\\ccn-lite\\src")
-        os.system("dir")
+    def __init__(self,path):
+        command = path+"/build/bin/ccn-lite-relay -v90 -u 9000"
+        try:
+            df = subprocess.check_call(command, stdout=PIPE,shell=True)
+            output, err = df.communicate()
+        except CalledProcessError as e:
+            print("error")
+            print(e)
 
 if __name__=="__main__":
     start = StartCCN()
--- Packet_Fuzzing.py
+++ Packet_Fuzzing.py
@@ -6,6 +6,9 @@ import socket
 from Generation import PacketMaker
 from Generation import Encode
 import random
+import os
+import sys
+from CCNStart import StartCCN
 
 if __name__ == '__main__':
     ccn = ['ccn', 'ccn-lite']
@@ -15,10 +18,17 @@ if __name__ == '__main__':
     parser.add_argument('path', help="path to the parser on this machine")
     args = parser.parse_args()
 
+    if (not os.path.exists(args.path)):
+        errstring = "The path "+args.path+" does not exist on this machine"
+        sys.exit(errstring)
+
     # TODO start parser
+
     if args.parser in ccn:
         print("ccn invoked")
         print("with path ", args.path)
+        start = StartCCN.StartCCN(args.path)
+
     elif args.parser in pycn:
         print("pycn invoked")
         print("with path ", args.path)
@@ -26,7 +36,7 @@ if __name__ == '__main__':
     time.sleep(5)
 
     # TODO Check if port 9000 is also used on PyCN-lite
-    sender = Sender(socket.gethostbyname(socket.gethostname()), 9000)
+    #sender = Sender("127.0.0.1", 9000)
     history = []
 
     # TODO Check if parser is still running
@@ -34,6 +44,7 @@ if __name__ == '__main__':
         # loop
         package = PacketMaker.makePackage[random.choice(list(PacketMaker.Packages))]
         bytes = Encode.encodePackage(package)
+        sender.sendMessage(bytes)
         history.append((package, bytes.hex()))
         time.sleep(1)
         print(history)
